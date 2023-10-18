088ea565f1872d73280be9a98b852707463cfe24
Dan Hudson <dan.hudsond@dtc.ox.ac.uk>
2023-10-18 09:39:23 +0100

    Add tcrdist OOM sh

---
+++ tcrdist_oom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --time=24:00:00 
+#SBATCH --ntasks=1
+#SBATCH --mem=30G
+#SBATCH --mail-user=dan.hudson@dtc.ox.ac.uk  # adjust this to match your email address
+#SBATCH --mail-type=ALL
+#SBATCH --output=/well/fernandes/projects/software/slurm/DH/%j.out
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+#module load MUSCLE/5.0.1428-GCCcore-10.3.0 # Loads MUSCLE for sequence alignment 
+export PIP_CACHE_DIR="../.pipcache" # Workaround to ensure library does not overload memory
+source /well/fernandes/projects/software/miniconda3/bin/activate
+#source venv/bin/activate # Activate the local environment
+conda activate tcr_scapes
+
+module list
+echo "Loaded modules and activated virtual environment"
+echo "Running"
+
+python run.py -m tcrdist3 -tcdc True -ds 1000 -ex "tcrdist OOM check" -no 10000
+python run.py -m tcrdist3 -ds 1000 -ex "tcrdist OOM check" -no 10000
+python run.py -m tcrdist3 -tcds  -ds 1000 -ex "tcrdist OOM check" -no 10000
+
+conda deactivate
+conda deactivate
+module purge
+echo "DONE"
+
